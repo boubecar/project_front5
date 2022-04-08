6289dadf686ba77ba2b3618d4c53410aefe8a926
mariem202 <82376491+mariem202@users.noreply.github.com>
2022-04-08 17:38:58 +0100

    quizz

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-note',
@@ -7,25 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteComponent implements OnInit {
 
-  constructor() { }
-  public counts = ["Recieved","In Progress","Ready for Billing",
+  constructor(private router: Router) { }
+  /*public counts = ["Recieved","In Progress","Ready for Billing",
   "Billed","Order Closed"];
-  public orderStatus = "In Progress"
+  public orderStatus = "In Progress"*/
+  currentQuiz =0;
+  List: Array<{ Norme: string, critere: string }> = [
+    { Norme: "Nettoyer", critere: "État et propreté du poste de travail" },
+    { Norme: "Nettoyer", critere: 'État et propreté des installations' },
+    { Norme: "Nettoyer", critere: 'État et propreté des sols (papiers, chiffons éparpillés, stagnation d’eau, poussières)' },
+    { Norme: "Nettoyer", critere: "État et propreté des abords /vitres, murs, fenêtres..." },
+    { Norme: "Nettoyer", critere: "État et propreté du mobilier, des armoires et des casiers personnels" },
+  ];
 
   ngOnInit(): void {
   }
-/* progress bar 
-var progress = setInterval(function() {
-	var $bar = $('.bar');
-
-   	if ($bar.width()==400) {
-        clearInterval(progress);
-      	$bar.width(0);
-    } else {
-        $bar.width($bar.width()+40);
+  Suiv ()
+  {
+    if (this.currentQuiz<this.List.length)
+    {
+      this.currentQuiz++;
     }
-    $bar.text($bar.width()/4 + "%");
+    else 
+    {
+      /*this._service.updatePatient(this.patient).subscribe(
+        data => {*/
+      this.router.navigateByUrl('/evaluation');
+    }
+  }
   
-}, 800);
-/**********************/
 }
