2485730104b0e4da735f28706fbadeae3a8b04a9
mariem202 <82376491+mariem202@users.noreply.github.com>
2022-04-14 16:27:19 +0100

    ev

--- src/app/evaluation/evaluation.component.ts
+++ src/app/evaluation/evaluation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CritereService } from '../services/critere.service';
 import { NormeServiceService } from '../services/norme-service.service';
 
 @Component({
@@ -7,8 +8,9 @@ import { NormeServiceService } from '../services/norme-service.service';
   styleUrls: ['./evaluation.component.css']
 })
 export class EvaluationComponent implements OnInit {
- //NormeList: any = []
-  NormeList: Array<{NormeId: number, designation: string,path:string}> = [
+ NormeList: any = []
+ CritereList: any = []
+ /* NormeList: Array<{NormeId: number, designation: string,path:string}> = [
     {NormeId: 1, designation: "Nettoyer",path: 'fa-paint-brush'},
     {NormeId: 2, designation: 'Ranger',path: 'fa-cubes'},
     {NormeId: 3, designation: 'Etre rigoureux',path: 'fa-balance-scale'},
@@ -22,16 +24,23 @@ CritereList: Array<{ critereId: number, criterelabel: string, normes: string }> 
   { critereId: 3, criterelabel: 'Etre rigoureux', normes: "" },
   { critereId: 4, criterelabel: "Maintenir l'ordre", normes: "" },
   { critereId: 5, criterelabel: "Débarrasser", normes: "" },
-];
-  constructor(public normeService: NormeServiceService) { }
+];*/
+  constructor(public normeService: NormeServiceService,public CritereService: CritereService) { }
 
   ngOnInit(): void {
   }
 
   refreshnormList() {
     this.normeService.getListNormes().subscribe(data => {
-      //this.NormeList = data;
+      this.NormeList = data;
     });
   }
+  refreshcriList(e:any) {
+    this.CritereService.getCritereByNorme(e.NormeId).subscribe(data => {
+      this.CritereList = data;
+      console.log(this.CritereList)
+    });
 
+
+  }
 }
